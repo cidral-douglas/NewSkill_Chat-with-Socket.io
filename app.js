d328174a2e8b58d0a38de324b721faea51fd442b
Douglas Cidral <doug.coding20@gmail.com>
2020-09-28 11:12:59 -0300

    Adding the Rooms

--- app.js
+++ app.js
@@ -3,21 +3,48 @@ const app = express();
 const path = require('path');
 const socketIO = require('socket.io');
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/gp1', express.static(path.join(__dirname, 'public')));
+app.use('/gp2', express.static(path.join(__dirname, 'public')));
 
 const server = app.listen(3000, ()=>{ console.log("Running") });
 
-const messages = [];
+const messages = { gp1: [], gp2: [] };
 
 const io = socketIO(server);
 
-io.on('connection', (socket)=> {
+const gp1 = io.of('/gp1').on('connection', (socket)=> {
     console.log('new connection');
-    socket.emit('update_messages', messages);
+    socket.emit('update_messages', messages.gp1);
 
     socket.on('new_message', (data)=> {
-        messages.push(data);
+        messages.gp1.push(data);
         console.log(messages);
-        io.emit('update_messages', messages);
-    })
+        gp1.emit('update_messages', messages.gp1);
+    });
 })
+
+const gp2 = io.of('/gp2').on('connection', (socket)=> {
+    console.log('new connection');
+    socket.emit('update_messages', messages.gp2);
+
+    socket.on('new_message', (data)=> {
+        messages.gp2.push(data);
+        console.log(messages);
+        gp2.emit('update_messages', messages.gp2);
+    });
+})
+
+
+
+
+
+// io.on('connection', (socket)=> {
+//     console.log('new connection');
+//     socket.emit('update_messages', messages);
+
+//     socket.on('new_message', (data)=> {
+//         messages.push(data);
+//         console.log(messages);
+//         io.emit('update_messages', messages);
+//     })
+// })
